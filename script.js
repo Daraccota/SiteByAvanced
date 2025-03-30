767d643ff521dd1269e15ef163e9c2bbc8cfd3ec
Daraccota <dacidsouza@gmail.com>
2025-03-30 16:48:25 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-text", {
+      strings: [
+        "Criamos soluções inovadoras...",
+        "Transformamos ideias em tecnologia...",
+        "O futuro começa com a ByAvanced!"
+      ],
+      typeSpeed: 50, // Velocidade de digitação
+      backSpeed: 30, // Velocidade de apagar
+      backDelay: 2000, // Tempo antes de apagar
+      startDelay: 500, // Tempo antes de começar
+      loop: true // Faz o efeito repetir
+    });
+  });
+</script>
